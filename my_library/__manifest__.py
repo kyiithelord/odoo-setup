500dc812de7e0aecc9dcf5094f7d301c221410cb
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-11 00:52:33 -0400

    module1

---
+++ my_library/__manifest__.py
@@ -0,0 +1,14 @@
+{
+        'name': "My library",
+        'summary': "Manage books easily",
+        'author': "Innovixer",
+        'website': "http://www.innovixer.com",
+        'category': 'Uncategorized',
+        'version': '16.0.1',
+        'depends': ['base'],
+        'data': [
+            'views/library_book.xml',
+            'security/ir.model.access.csv'
+        ],
+        # 'demo': ['demo.xml'],
+}
