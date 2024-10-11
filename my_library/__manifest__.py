f59c950d1cead2b6c16937b55af0487092207452
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-11 05:21:55 -0400

    update

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -4,11 +4,8 @@
         'author': "Innovixer",
         'website': "http://www.innovixer.com",
         'category': 'Uncategorized',
-        'version': '16.0.1',
-        'depends': ['base'],
         'data': [
             'views/library_book.xml',
             'security/ir.model.access.csv'
-        ],
-        # 'demo': ['demo.xml'],
+        ]
 }
